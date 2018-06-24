19c63e4dae8d943e4af76b122fcdd9f699322d28
Terrence <owusuterrence@gmail.com>
2018-06-24 18:15:43 +0100

    created reducers and actions for redux state

---
+++ client/reducers/index.js
@@ -0,0 +1,5 @@
+export const FETCH_USERS = 'fetch_users';
+export const FETCH_CURRENT_USER = 'fetch_current_users';
+export const FETCH_ADMINS = 'fetch_admins';
+
+
---
+++ helpers/createStore.js
@@ -0,0 +1,7 @@
+import { createStore, applyMiddleware } from 'redux'
+
+
+
+const store = createStore(
+    
+)

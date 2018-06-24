bb35f2cc3693c029da483e13dbee163df3c46909
Terrence <owusuterrence@gmail.com>
2018-06-24 17:45:10 +0100

    created a client.js file render the served html from the server

---
+++ client/client.js
@@ -0,0 +1,12 @@
+// Starting point of client side application
+
+import 'babel-polyfill';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+ReactDOM.render(
+    <div>Hello</div>,
+    document.querySelector('#root')
+)

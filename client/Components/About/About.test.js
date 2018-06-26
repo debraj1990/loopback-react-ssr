0b68acc33ec10fafe326fbc90cb73b724c919366
Terrence <owusuterrence@gmail.com>
2018-06-26 09:42:09 +0100

    added enzyme adapter and updated the package json

--- client/Components/About/About.test.js
+++ client/Components/About/About.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import { shallow } from 'enzyme'
+import { shallow, configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16';
 import toJson from 'enzyme-to-json'
 import About from './'
-import Adapter from 'enzyme-adapter-react-16';
 
-Enzyme.configure({ adapter: new Adapter() });
+
+configure({adapter: new Adapter });
 
 
 test('About component should render as expected', () => {
